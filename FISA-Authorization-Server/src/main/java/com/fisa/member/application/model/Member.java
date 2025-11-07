387d631a8cd013ccd18ca94d4df7271bca98fece
Bellmin <whdals011203@naver.com>
2025-11-07 21:56:38 +0900

    feat: load static 메소드 추가

--- FISA-Authorization-Server/src/main/java/com/fisa/member/application/model/Member.java
+++ FISA-Authorization-Server/src/main/java/com/fisa/member/application/model/Member.java
@@ -5,6 +5,8 @@ import com.fisa.member.application.model.profile.Curriculum;
 import com.fisa.member.application.model.profile.Email;
 import com.fisa.member.application.model.profile.MemberName;
 import com.fisa.member.application.model.profile.PhoneNumber;
+import java.util.UUID;
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -19,7 +21,7 @@ public class Member {
     private final Curriculum curriculum;
     private final AuthInfo authInfo;
 
-    @Builder
+    @Builder(access = AccessLevel.PRIVATE)
     private Member(
                    MemberId id,
                    MemberName name,
@@ -35,6 +37,18 @@ public class Member {
         this.email = email;
         this.name = name;
         this.authInfo = authInfo;
+    }
+
+    public static Member load(UUID memberId, String name, String phoneNumber, int generation, String email, Curriculum curriculum, String loginId, String credential){
+        return Member.builder()
+                .authInfo(AuthInfo.create(loginId, credential))
+                .id(MemberId.load(memberId))
+                .phoneNumber(PhoneNumber.of(phoneNumber))
+                .generation(generation)
+                .email(Email.of(email))
+                .name(MemberName.of(name))
+                .curriculum(curriculum)
+                .build();
     }
 
     public static Member create(String name, String phoneNumber, int generation, String email, String curriculum, String loginId, String credential){
