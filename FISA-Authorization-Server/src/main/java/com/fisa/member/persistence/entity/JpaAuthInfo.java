78fe91a369616fc2b2d11af8db7a107cbd0e6627
Bellmin <whdals011203@naver.com>
2025-11-07 23:47:20 +0900

    feat: 테이블 인덱스 추가

--- FISA-Authorization-Server/src/main/java/com/fisa/member/persistence/entity/JpaAuthInfo.java
+++ FISA-Authorization-Server/src/main/java/com/fisa/member/persistence/entity/JpaAuthInfo.java
@@ -1,12 +1,15 @@
 package com.fisa.member.persistence.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Index;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -18,14 +21,22 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
 @Builder(access = AccessLevel.PRIVATE)
+@Table(
+        name = "member_auth_info",
+        indexes = {
+                @Index(name = "idx_member_auth_info", columnList = "login_id")
+        }
+)
 public class JpaAuthInfo {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false, unique = true)
     private String loginId;
 
+    @Column(nullable = false)
     private String credential;
 
     @OneToOne(fetch = FetchType.EAGER)
--- FISA-Authorization-Server/src/main/java/com/fisa/member/persistence/entity/JpaMember.java
+++ FISA-Authorization-Server/src/main/java/com/fisa/member/persistence/entity/JpaMember.java
@@ -7,8 +7,10 @@ import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Index;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
 import java.util.UUID;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -21,6 +23,13 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
 @Builder(access = AccessLevel.PRIVATE)
+@Table(
+        name = "member",
+        indexes = {
+                @Index(name = "idx_member_name", columnList = "name"),
+                @Index(name = "idx_member_email", columnList = "email")
+        }
+)
 public class JpaMember {
 
     @Id
