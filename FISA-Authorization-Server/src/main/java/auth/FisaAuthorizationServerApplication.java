aa9d394967daa5321c91064ec33afcca7d76e841
Bellmin <whdals011203@naver.com>
2025-11-07 20:26:29 +0900

    chore: 초기 설정

---
+++ FISA-Authorization-Server/src/main/java/auth/FisaAuthorizationServerApplication.java
@@ -0,0 +1,13 @@
+package auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FisaAuthorizationServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FisaAuthorizationServerApplication.class, args);
+    }
+
+}
